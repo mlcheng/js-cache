d9ed4ba164aff2c2d2e0d7e95cf28ceb2b81a268
Michael Cheng <michael@michaelcheng.us>
2016-07-24 11:36:47 +0800

    First commit

---
+++ cache.js
@@ -0,0 +1,151 @@
+/***********************************************
+
+  "cache.js"
+
+  Created by Michael Cheng on 07/24/2016 09:44
+            http://michaelcheng.us/
+            michael@michaelcheng.us
+            --All Rights Reserved--
+
+***********************************************/
+
+'use strict';
+
+var iqwerty = iqwerty || {};
+
+iqwerty.cache = (function() {
+
+	function LruCacheNode(value) {
+		this.prev = null;
+		this.next = null;
+		this.value = value;
+	}
+
+	function LinkedList() {
+		this.head = null;
+		this.tail = null;
+		let list = this;
+
+		return {
+			enqueue(lruCacheNode) {
+				if(list.head === null) {
+					list.head = lruCacheNode;
+					list.tail = lruCacheNode;
+				} else {
+					lruCacheNode.prev = list.tail;
+					list.tail.next = lruCacheNode;
+					list.tail = lruCacheNode;
+				}
+			},
+			dequeue() {
+				if(list.head) {
+					let out = list.head;
+
+					list.head.next.prev = null;
+					list.head = list.head.next;
+					out.next = null;
+					return out;
+				}
+				return null;
+			},
+			promote(lruCacheNode) {
+				if(list.tail === lruCacheNode) return; // Already promoted
+
+				if(list.head === lruCacheNode) {
+					list.head.next.prev = null;
+					list.head = list.head.next;
+					lruCacheNode.next = null;
+					lruCacheNode.prev = list.tail;
+					list.tail.next = lruCacheNode;
+					list.tail = lruCacheNode;
+				} else {
+					lruCacheNode.prev.next = lruCacheNode.next;
+					lruCacheNode.next.prev = lruCacheNode.prev;
+					lruCacheNode.prev = list.tail;
+					lruCacheNode.next = null;
+					list.tail.next = lruCacheNode;
+					list.tail = lruCacheNode;
+				}
+			},
+			export() {
+				let iterator = list.head, out = [];
+				while(iterator) {
+					out.push(iterator.value);
+					iterator = iterator.next;
+				}
+
+				return out;
+			}
+		};
+	}
+
+	function Builder(identifier, size) {
+		return LruCache(identifier, size);
+	}
+
+	Builder.prototype.__cache__ = {};
+
+	function LruCache(identifier, size) {
+		let exports = {};
+		let cache = Builder.prototype.__cache__;
+
+		exports.add = function(key, value) {			
+			// Create the cache if it doesn't exist yet
+			if(!cache.hasOwnProperty(identifier)) {
+				cache[identifier] = {
+					lru: new LinkedList(),
+					map: {}
+				};
+			}
+
+			if(cache[identifier].map.hasOwnProperty(key)) {
+				// Already exists; simply update the value in the lruCacheNode
+				let _co = cache[identifier].map[key];
+				_co.value.value = value;
+				cache[identifier].lru.promote(_co);
+			} else {
+				if(Object.keys(cache[identifier].map).length + 1 > size) {
+					// "Invariance", thanks Carl!
+					let removed = cache[identifier].lru.dequeue();
+					delete cache[identifier].map[removed.value.key];
+				}
+				let lruCacheNode = new LruCacheNode({ key, value });
+
+				// Add the cache node to the linked list
+				cache[identifier].lru.enqueue(lruCacheNode);
+
+				// Add a reference to the node to the hash map for quick access, but takes O(n) space
+				cache[identifier].map[key] = lruCacheNode;
+			}
+		};
+
+		exports.get = function(key) {
+			let _co = cache[identifier];
+			if(!_co.map.hasOwnProperty(key)) return undefined;
+
+			_co.lru.promote(_co.map[key]);
+			return _co.map[key].value.value;
+		};
+
+		exports.valueExists = value => Object.keys(cache[identifier].map)
+			.some(key => cache[identifier].map[key].value.value === value);
+
+		exports.purge = function() {
+			delete cache[identifier];
+		};
+
+		exports.export = function() {
+			if(cache.hasOwnProperty(identifier)) {
+				return cache[identifier].lru.export();
+			} else {
+				return undefined;
+			}
+		};
+
+		return exports;
+	}
+
+	return {
+		Builder
+	};
+})();
---
+++ tests/index.js
@@ -0,0 +1,58 @@
+/***********************************************
+
+  "index.js"
+
+  Created by Michael Cheng on 07/24/2016 11:15
+            http://michaelcheng.us/
+            michael@michaelcheng.us
+            --All Rights Reserved--
+
+***********************************************/
+
+'use strict';
+
+/* globals require, __dirname, iqwerty */
+const { Test, inject } = require('../../test/test.js');
+inject(__dirname, '../cache.js');
+
+
+let cache = iqwerty.cache.Builder('test', 5);
+Test('Basic adding to cache works')
+	.do(() => {
+		cache.add('k1', 'v1');
+		cache.add('k2', 'v2');
+	})
+	.expect(cache.get('k1'))
+	.toBe('v1');
+
+Test('Accessing a value promotes it to the end of the queue')
+	.do(() => cache.get('k2'))
+	.expect(cache.export()[cache.export().length-1].value)
+	.toBe('v2');
+
+Test('The value "v2" should exist')
+	.assert(cache.valueExists('v2'))
+	.is(true);
+
+Test('Can add up to the specified size limit of objects to cache')
+	.do(() => {
+		cache.add('k3', 'v3');
+		cache.add('k4', 'v4');
+		cache.add('k5', 'v5');
+	})
+	.expect(cache.export().length)
+	.toBe(5);
+
+Test('Adding another object will cause the least recently used object to be deleted')
+	.do(() => {
+		cache.add('k6', 'v6');
+	})
+	.expect(cache.get('k1'))
+	.toBe(undefined);
+
+Test('Purging cache will remove all related cache')
+	.do(() => {
+		cache.purge();
+	})
+	.expect(cache.export())
+	.toBe(undefined);
